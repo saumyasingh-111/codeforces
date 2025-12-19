46d07ca1b5ed847e512199369739284e7843eabd
saumya singh <saumyasingh3234@gmail.com>
2025-12-19 12:32:16 +0530

    adding codechef ques 1

---
+++ dec19ccques1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // your code goes here
+    int x, y;
+    cin >> x >> y;
+    if (x > y) {
+        cout << "no";
+    } else {
+        cout << "yes";
+    }
+}
