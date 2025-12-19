f1df99fc8cd00c24fc98a89d8b73604b4e9eef9a
saumya singh <saumyasingh3234@gmail.com>
2025-12-19 12:48:50 +0530

    adding codechef ques 5

---
+++ dec19ques5.cpp
@@ -0,0 +1,13 @@
+// https://www.codechef.com/problems/GAMBLING
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // your code goes here
+    int a;
+    cin >> a;
+    cout << 7 - a;
+}
