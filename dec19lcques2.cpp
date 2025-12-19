df68875476c37227dfa8d588651d9a0e08ac21f0
saumya singh <saumyasingh3234@gmail.com>
2025-12-19 12:22:31 +0530

    minor changes

--- dec19lcques2.cpp
+++ dec19lcques2.cpp
@@ -1,33 +1,18 @@
+136. Single Number
 #include <iostream>
 #include <vector>
 #include <algorithm>
 using namespace std;
 int main(){
+
 class Solution {
 public:
-    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int total=n*n;
-
-        vector<int> freq(total + 1, 0);
-         
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                freq[grid[i][j]]++;
-            }
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for(int x : nums){
+            ans ^= x;
         }
-
-        int repeating=-1, missing=-1;
-
-        for(int i=1;i<=total;i++){
-            if(freq[i]==2){
-                repeating=i;
-            }
-            else if(freq[i]==0){
-                missing=i;
-            }
-        }
-         return {repeating, missing};
+        return ans;
     }
-}
+};
 }
