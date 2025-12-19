87487706c7cdbf4ff72dbce8d05cca7374943c65
saumya singh <saumyasingh3234@gmail.com>
2025-12-19 12:46:00 +0530

    adding codechef ques 4

---
+++ dec19ccques4.cpp
@@ -0,0 +1,18 @@
+// https://www.codechef.com/problems/CLSI
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	// your code goes here
+int n,b,iq;
+cin>>n>>b;
+iq=10*b;
+if(n>=iq){
+    cout<<"yes";
+}else{
+    cout<<"no";
+}
+}
