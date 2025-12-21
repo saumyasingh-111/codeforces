83b1c528610806482cfe5629f3e5da968d475ed0
saumya singh <saumyasingh3234@gmail.com>
2025-12-21 07:33:36 +0530

    ques 2 B

---
+++ dec21cfques2.cpp
@@ -0,0 +1,20 @@
+// ques 2 B  round 952 div 4
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if(n==3){
+            cout<<3<<"\n";
+        }else{
+            cout<<2<<"\n";
+        }
+    }
+}
