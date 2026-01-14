2dc202a8dc31c7dcb749328859e4e14b5be784ec
saumya singh <saumyasingh3234@gmail.com>
2026-01-14 11:44:59 +0530

    added file

---
+++ Div-3/round 1071/A.cpp
@@ -0,0 +1,15 @@
+// // Blackslex and Password
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int k,x;
+        cin>>k>>x;
+        cout<<k*x+1<<"\n";
+    }
+}
