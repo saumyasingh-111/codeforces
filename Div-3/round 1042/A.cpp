81690d13d5d584460d9a681946343f8c8a4be594
saumya singh <saumyasingh3234@gmail.com>
2026-01-14 11:11:38 +0530

    added file

---
+++ Div-3/round 1042/A.cpp
@@ -0,0 +1,30 @@
+// lever
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> a(n), b(n);
+        for (int i = 0; i < n; i++) cin >> a[i];
+        for (int i = 0; i < n; i++) cin >> b[i];
+
+        int excess = 0;
+        for (int i = 0; i < n; i++) {
+            if (a[i] > b[i])
+                excess += a[i] - b[i];
+        }
+
+        cout << excess + 1 << "\n";
+    }
+}
